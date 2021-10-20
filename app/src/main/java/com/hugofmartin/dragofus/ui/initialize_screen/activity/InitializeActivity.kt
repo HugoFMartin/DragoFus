e86b0e1ce0ed25680230172973eaab63e928937d
HugoFMartin <hugofabien.martin@gmail.com>
2021-10-20 18:42:24 +0200

    feature: create initialize screens - add fragments and navigation

---
+++ app/src/main/java/com/hugofmartin/dragofus/ui/initialize_screen/activity/InitializeActivity.kt
@@ -0,0 +1,12 @@
+package com.hugofmartin.dragofus.ui.initialize_screen.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hugofmartin.dragofus.R
+
+class InitializeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initialize)
+    }
+}
---
+++ app/src/main/java/com/hugofmartin/dragofus/ui/initialize_screen/fragment/InitializeEndFragment.kt
@@ -0,0 +1,22 @@
+package com.hugofmartin.dragofus.ui.initialize_screen.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hugofmartin.dragofus.R
+
+class InitializeEndFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.initializing_end_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

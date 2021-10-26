199442cc921ec45389b9c6e923c4b12327078514
HugoFMartin <hugofabien.martin@gmail.com>
2021-10-26 19:55:08 +0200

    feature: init-main-screen - add activity and navigation

---
+++ app/src/main/java/com/hugofmartin/dragofus/ui/main_screen/activity/DragodindeActivity.kt
@@ -0,0 +1,12 @@
+package com.hugofmartin.dragofus.ui.main_screen.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hugofmartin.dragofus.R
+
+class DragodindeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dragodinde)
+    }
+}
